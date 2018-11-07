121276b635aa9407b7840a521b054162c99926b1
mutianyan <mutianyan@econ1-188-150-dhcp.int.colorado.edu>
2018-11-07 15:19:14 -0700

    adding attribute dictionaries to convert to numeric values

--- ProcessData.py
+++ ProcessData.py
@@ -8,6 +8,35 @@ def transforForamt(dataset, diction):
 
 data_set = pd.read_csv('data/WA_Fn-UseC_-Telco-Customer-Churn.csv')
 dic={"Female":1,"Male":0}
+
+
+dic_2={"Yes":1,"No":0}
+data_set['Partner'] = transforForamt(data_set['Partner'],dic_2)
+data_set['Dependents'] = transforForamt(data_set['Dependents'],dic_2)
+data_set['PhoneService'] = transforForamt(data_set['PhoneService'],dic_2)
+data_set['PaperlessBilling'] = transforForamt(data_set['PaperlessBilling'],dic_2)
+data_set['Churn'] = transforForamt(data_set['Churn'],dic_2)
+
+dic_3={"No internet service":-1,"Yes":1, "No":0}
+data_set['MultipleLines'] = transforForamt(data_set['MultipleLines'],dic_3)
+data_set['OnlineSecurity'] = transforForamt(data_set['OnlineSecurity'],dic_3)
+data_set['OnlineBackup'] = transforForamt(data_set['OnlineBackup'],dic_3)
+data_set['DeviceProtection'] = transforForamt(data_set['DeviceProtection'],dic_3)
+data_set['TechSupport'] = transforForamt(data_set['TechSupport'],dic_3)
+data_set['StreamingTV'] = transforForamt(data_set['StreamingTV'],dic_3)
+data_set['StreamingMovies'] = transforForamt(data_set['StreamingMovies'],dic_3)
+
+dic_4={"No":0,"DSL":1, "Fiber optic":3}
+data_set['InternetService'] = transforForamt(data_set['InternetService'],dic_4)
+
+
+dic_5={"Month-to-month":1,"One year":2, "Two year":3}
+data_set['Contract'] = transforForamt(data_set['Contract'],dic_5)
+
+dic_6={"Electric check":1,"Mailed check":2, "Bank transfer (automatic)":3, "Credit card (automatic)":4}
+data_set['PaymentMethod'] = transforForamt(data_set['PaymentMethod'],dic_6)
+
+
 data_set['gender'] = transforForamt(data_set['gender'],dic)
 data_suffle = data_set.sample(frac=1)
 train_ratio = 0.638
